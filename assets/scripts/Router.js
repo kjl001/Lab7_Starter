6bba05fb8464010b9f002939578e22ba2efdbdc8
Kevin Lee <kjl001@ucsd.edu>
2021-11-05 20:08:30 -0700

    expose

--- assets/scripts/Router.js
+++ assets/scripts/Router.js
@@ -38,6 +38,7 @@ export class Router {
      * router instance using the 'this' keyword. Substitute 'home' for the variable
      * page
      */
+    this[page] = pageFunc;
   }
 
   /**
@@ -65,5 +66,26 @@ export class Router {
      *     and URL + hash to history
      *  4. Finally, call the stored function for the given page
      */
+    try {
+      this[page];
+    }
+    catch(err) {
+      console.log("ERROR: Function does not exist");
+      return;
+    }
+
+    var hash = "";
+    if(page === "home") {
+      hash = "";
+    }
+    else {
+      hash = "#" + page;
+    }
+
+    if(!statePopped && window.location.hash != hash) {
+      history.pushState(page, "", "/" + hash);
+    }
+
+    this[page];
   }
 }
